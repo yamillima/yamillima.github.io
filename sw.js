bd1daa68a51bc7a9bbdea5703723dd90e8661bf6
Yamil Lima <yamilandreslima@gmail.com>
2024-10-30 17:14:10 -0500

    Add url target for notification clicks

--- sw.js
+++ sw.js
@@ -2,10 +2,19 @@ self.addEventListener('push', function(event) {
     const options = {
       body: event.data.text(),
       icon: '/icono.png',
-      badge: '/badge.png'
+      badge: '/badge.png',
+        data: { url: 'https://github.com' } // Agrega la URL o cualquier dato necesario
     };
     event.waitUntil(
       self.registration.showNotification('Título de la Notificación', options)
     );
   });
  
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close(); // Cierra la notificación
+  
+  // Abre una nueva ventana o pestaña con la URL especificada en la notificación
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
