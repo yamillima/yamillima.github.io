6a734dda2e4f3407828c8de0041f4d41ce4231f7
Yamil Lima <yamilandreslima@gmail.com>
2024-10-30 22:21:45 -0500

    Add only one direct notification target url

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('push', function(event) {
       body: event.data.text(),
       icon: '/icono.png',
       badge: '/badge.png',
-      data: event.data ? event.data.json() : {url : 'https://github.com'} // Agrega la URL o cualquier dato necesario
+      data: {url : 'https://github.com'} // Agrega la URL o cualquier dato necesario
     };
     event.waitUntil(
       self.registration.showNotification('Título de la Notificación', options)
@@ -14,7 +14,7 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close(); // Cierra la notificación
   
   // Abre una nueva ventana o pestaña con la URL especificada en la notificación
-  const url = event.notification.data.url;
+  const url = 'https://github.com';
   event.waitUntil(      
     clients.openWindow(url)
   );
